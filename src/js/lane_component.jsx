0e09a92f6df60cb5b03b61a0b3f3bcfd953e4479
xek6ae <xek6ae@googlemail.com>
2015-11-18 23:02:52 +0100

    Lane anzeigen aus mehreren Strings

--- src/js/lane_component.jsx
+++ src/js/lane_component.jsx
@@ -3,17 +3,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-var data = [{"seq1": "TGACTGACTGACTGACTGACTGACTGACTGACTGACTGACTGACTGACTGAC"},
-            {"seq2": "TGACTGACTGACTGACTGACTGACTGACTGACTGACTGACTGACTGACTGAC"}];
+var data = [{id: 1, text: "TGACTGACTGACTGACTGACTGACTGACTGACTGACTGACTGACTGACTGAC"},
+            {id: 2, text: "TGACTGACTGACTGACTGACTGACTGACTGACTGACTGACTGACTGACTGAC"},
+            {id: 3, text: "TGACTGACTGACTGACTGACTGACTGACTGACTGACTGACTGACTGACTGAC"},
+            {id: 4, text: "TGACTGACTGACTGACTGACTGACTGACTGACTGACTGACTGACTGACTGAC"},
+            {id: 5, text: "TGACTGACTGACTGACTGACTGACTGACTGACTGACTGACTGACTGACTGAC"}
+            ];
 
 var Lane = React.createClass({
-  getInitialState: function(){
-    return {data: []};
+  //functions?
+  parseSeq: function(seq){
+
+    return seq[1].text.slice(0,5)+"#"+seq[1].text.slice(6,22)+"#"+seq[1].text.slice(23);
   },
   render: function() {
     return (
-      <div className="commentBox">
-        {this.state.data[1]}
+      <div className="lane">
+        <p>
+          {this.props.data.map(function(row){
+              console.log(row.text.length);
+              return row.text;
+            })}
+        </p>
+        <p>
+          {this.parseSeq(this.props.data)}
+        </p>
       </div>
     );
   }
