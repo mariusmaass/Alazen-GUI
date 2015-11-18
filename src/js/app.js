5a19b50bcc634b2038c136d3825df56dfd5dbe40
Jan Lelis <mail@janlelis.de>
2015-11-19 00:14:09 +0100

    tidy up a little

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,4 @@
 'use strict';
 
 import React from 'react';
-import 'other_file';
-//import 'react_component.jsx!';
 import 'lane_component.jsx!';
-
-let testES6 = 'working';
-console.log('it is', testES6);
--- src/js/other_file.js
+++
@@ -1,3 +0,0 @@
-'use strict';
-
-console.log('hello world from another file');
