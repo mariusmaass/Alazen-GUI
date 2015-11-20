32c66025541f1e3b7e24d0283db887c1076a56bb
RobinPapke <robin_papke@web.de>
2015-11-20 17:04:29 +0100

    sendRequest()-function added

--- src/js/backend/json_handler.js
+++ src/js/backend/json_handler.js
@@ -1,12 +1,28 @@
-var refGen = require("../../../test/json/dummyRefGen.json")
+//var refGen = require("../../../test/json/dummyRefGen.json");
+//
+//console.log("refseq: " + refGen.refseq + "\n" + "begin: " + refGen.begin + "\n" + "end: " + refGen.end + "\n");
+//
+//var mutations = require("../../../test/json/dummyMutations.json");
+//
+//console.log("source: " + mutations.source + "\n" + "begin: " + mutations.begin + "\n" + "end: " + mutations.end + "\n" + "mutations: " + mutations.mutations[0].name);
 
-console.log("refseq: " + refGen.refseq + "\n" + "begin: " + refGen.begin + "\n" + "end: " + refGen.end + "\n")
+function sendRequest() {
+ $.ajax({
+     type: "GET",
+     url: "http://127.0.0.1:8000/dummyRefGen.json",
+     data: {},
+     dataType: "json",
+     success: function(data){
+    	 console.log(JSON.stringify(data));
+     },
+     error: function(){
+       alert("Error:");
+     }
+  });
+}
 
-var mutations = require("../../../test/json/dummyMutations.json")
-
-console.log("source: " + mutations.source + "\n" + "begin: " + mutations.begin + "\n" + "end: " + mutations.end + "\n" + "mutations: " + mutations.mutations[0].name)
-
-
+alert("Test");
+sendRequest();
 
 
 
