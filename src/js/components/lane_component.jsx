be9ad044e590f5b1106db0260dbbc110454df78d
Erik <elisathe@informatik.hu-berlin.de>
2015-11-23 23:30:31 +0100

    WIP: Lane Fragment erstellt / Fragemente einfärbbar

--- src/js/components/lane_component.jsx
+++ src/js/components/lane_component.jsx
@@ -2,7 +2,49 @@
 
 import React from 'react';
 
+var testjson = [{id: 1, sequence: "ATGCATGCATGCATGCATGC", mutation: false},
+                {id: 2, sequence: "BGFD", mutation: true},
+                {id: 3, sequence: "ATGCATGCATGCATGCATGC", mutation: false},
+                {id: 4, sequence: "TGACTGACTGACTGACTGACTGACTGACTGACTGACTGACTGACTGACTGAC", mutation: false},
+                {id: 5, sequence: "ATGCATGCATGCATGCATGC", mutation: false},
+                {id: 6, sequence: "===BG", mutation: true},
+                {id: 7, sequence: "ATGCATGCATGCATGCATGC", mutation: false}
+              ];
+
+var LaneFragment = React.createClass({
+  style: function(){
+    return {backgroundColor: this.props.color}
+  },
+  render: function() {
+    return (
+      <span className="lanefragment" style={this.style()}>
+        {this.props.sequence}
+      </span>
+    );
+  }
+});
+
 var Lane = React.createClass({
+  getInitialState: function() {
+    return {colors: ["red", "green"],
+            testseq: testjson};
+  },
+  setLanes: function() {
+    return this.state.colors.map(function(color){
+      return <LaneFragment sequence="XYW" color={color}/>
+    });
+  },
+  setLane: function() {
+    return this.state.testseq.map(function(seq){
+      var color;
+      if(seq.mutation){
+        color = "red";
+      }else{
+        color = "green";
+      }
+      return <LaneFragment sequence={seq.sequence} color={color} />
+    })
+  },
   //functions?
   parseSeq: function(seq){
 
@@ -19,6 +61,13 @@ var Lane = React.createClass({
         </p>
         <p>
           {this.parseSeq(this.props.data)}
+          <LaneFragment sequence="XYZ" color={this.state.colors}/>
+        </p>
+        <p>
+          {this.setLanes()}
+        </p>
+        <p>
+          {this.setLane()}
         </p>
       </div>
     );
