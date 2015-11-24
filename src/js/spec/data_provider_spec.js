ecd9aa22968a73dffba670bcb1b8f2a2293b961e
lrebscher <lucas.rebscher@sprylab.com>
2015-11-24 22:01:46 +0100

    add more test-cases to data_provider_spec

--- src/js/spec/data_provider_spec.js
+++ src/js/spec/data_provider_spec.js
@@ -7,5 +7,14 @@ describe('dataProvider', function(){
     it('has a fetchMutations function', function(){
       expect( dataProvider.fetchMutations ).toBeDefined();
     });
+
+    it('has a fetchReferenceGenome function', function()  {
+    	expect( dataProvider.fetchReferenceGenome ).toBeDefined();
+    });
+
+    it('has a searchFor function', function() {
+    	expect ( dataProvider.searchFor ).toBeDefined();
+    });
+
   });
 });
