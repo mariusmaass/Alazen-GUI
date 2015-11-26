986cee624fd791a7f209c1e82640d445eaab6653
Jan Lelis <mail@janlelis.de>
2015-11-26 23:16:05 +0100

    drafting some general markup/css structure

--- src/js/components/searchfield_component.jsx
+++ src/js/components/searchfield_component.jsx
@@ -40,7 +40,7 @@ var SearchField = React.createClass({
 				<div idName="custom-search-input">
                     <div className="input-group col-md-12">
                     	
-                        <input type="text" className="search-query form-control" placeholder="Search" ref="inputSearch" />
+                        <input type="text" className="search-query form-control" placeholder="Search Mutation or Position" ref="inputSearch" />
                         <span className="input-group-btn">
                             <button className="btn btn-danger" type="button" onClick={this.handleSubmit}>
                                 <span className="glyphicon glyphicon-search"></span>
