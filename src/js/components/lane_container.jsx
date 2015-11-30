a085b95e8e24fe3165c73f71b5925c7a83044763
Jan Lelis <mail@janlelis.de>
2015-11-30 20:21:47 +0100

    diagram w/ react-d3

--- src/js/components/lane_container.jsx
+++ src/js/components/lane_container.jsx
@@ -3,18 +3,19 @@
 import React from 'react';
 
 import LaneComponent from './lane_component.jsx!';
+import LaneAggregation from './lane_aggregation.jsx!';
 
 var LaneContainer = React.createClass({
-  createLane: function(){
+  createLanes: function(){
     return this.props.data.map(function(lanedata){
       return <LaneComponent data={lanedata.data} />
     })
   },
   render: function(){
     return <div className="lanecontainer">
-      {this.createLane()}
+      {this.createLanes()}
+      <LaneAggregation key="lane-aggregation-example" />
     </div>
-
   }
 });
 
