8a6f666f68463e9b1065ecfebcc81356f1e1a71d
lrebscher <lucas.rebscher@sprylab.com>
2015-12-06 14:59:10 +0100

    add dummyBackendServer, dataProvider will now communicate with dummyBackendServer

--- src/js/backend/data_provider.js
+++ src/js/backend/data_provider.js
@@ -2,17 +2,17 @@
 
 import $ from 'jquery';
 
-var refGenUrl = "/test/dummyRefGen.json";
-var mutationsUrl = "/test/dummyMutations.json";
+var refGenUrl = "http://localhost:8081/refGen/";
+var mutationsUrl = "http://localhost:8081/mutations/";
 
 function fetchData(url) {
  $.ajax({
      type: "GET",
      url: url,
      data: {},
-     dataType: "json",
+     dataType: "jsonp",
      success: function(data){
-       console.log(JSON.stringify(data));
+       console.log(JSON.parse(data));
      },
      error: function(e){
        console.log("Error:", e);
@@ -21,20 +21,23 @@ function fetchData(url) {
 }
 
 var dataProvider = {
-  
+
   fetchReferenceGenome: function() {
     return fetchData(refGenUrl);
   },
-  
+
   fetchMutations: function() {
     return fetchData(mutationsUrl);
   },
-  
+
   //returns position in source
   searchFor: function(source, reference) {
     //will be replaced by a post-request
     return {"begin" : "5000", "end" : "100000"};
   }
 }
+
+fetchData(refGenUrl);
+fetchData(mutationsUrl);
 
 export default dataProvider;
---
+++ src/js/backend/dummy_backend_server.js
@@ -0,0 +1,35 @@
+/*
+* using express (web-framework for node.js)
+* express needs to be installed -> node install express
+* then run 'node dummy_backend_server.js' to start the server
+*/
+var express = require('express');
+var app = express();
+var fs = require("fs");
+
+console.log("starting dummy backend...");
+
+app.get('/refGen', function (req, res) {
+      fs.readFile("../../static/test/dummyRefGen.json", 'utf8', function (err, data) {
+      console.log(data);
+      res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1');
+      res.jsonp(JSON.stringify(data));
+      res.end();
+   });
+})
+
+app.get('/mutations', function (req, res) {
+      fs.readFile("../../static/test/dummyMutations.json", 'utf8', function (err, data) {
+       console.log(data);
+       res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1');
+       res.jsonp(JSON.stringify(data));
+       res.end();
+   });
+})
+
+var server = app.listen(8081, 'localhost', function () {
+
+  var host = server.address().address
+  var port = server.address().port
+  console.log("dummy-backend-server listening at http://%s:%s", host, port)
+})
