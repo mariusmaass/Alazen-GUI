867d2f7417861317dc37f2dcfaa895846fee7f82
AleksandarAtanasov91 <aleksandar_atanasov91@abv.bg>
2015-12-12 16:28:14 +0100

    added a drop-down-menue[issue #12]

--- src/js/components/gui_component.jsx
+++ src/js/components/gui_component.jsx
@@ -5,6 +5,7 @@ import React from 'react';
 import LaneContainer from './lane_container.jsx!';
 import Slider from 'rc-slider';
 import SearchField from './searchfield_component.jsx!';
+import SelectChromosome from 'components/chromosome_selection_component.jsx!';
 
 var testjson = [{id: 1, sequence: "ATGCATGCATGCATGCATGC", mutation: false},
                 {id: 2, sequence: "BGFD", mutation: true},
@@ -24,6 +25,16 @@ var testdatas = [{id: 1, data: testjson},
                  {id: 2, data: testjson},
                  {id: 3, data: testjson}
                 ];
+
+var chromosomeList = [{
+  id: 1, name: "Chromosome 1"}, {id: 2, name: "Chromosome 2"}, {id: 3, name: "Chromosome 3"}, {
+  id: 4, name: "Chromosome 4"}, {id: 5, name: "Chromosome 5"}, {id: 6, name: "Chromosome 6"}, {
+  id: 7, name: "Chromosome 7"}, {id: 8, name: "Chromosome 8"}, {id: 9, name: "Chromosome 9"}, {
+  id: 10, name: "Chromosome 10"}, {id: 11, name: "Chromosome 11"}, {id: 12, name: "Chromosome 12"}, {
+  id: 13, name: "Chromosome 13"}, {id: 14, name: "Chromosome 14"}, {id: 15, name: "Chromosome 15"}, {
+  id: 16, name: "Chromosome 16"}, {id: 17, name: "Chromosome 17"}, {id: 18, name: "Chromosome 18"}, {
+  id: 19, name: "Chromosome 19"}, {id: 20, name: "Chromosome 20"}, {id: 21, name: "Chromosome 21"}, {
+  id: 22, name: "Chromosome 22"}, {id: 23, name: "Chromosome X"}, {id: 24, name: "Chromosome Y"}];
 
 var GuiComponent = React.createClass({
   getInitialState: function(){
@@ -45,10 +56,12 @@ var GuiComponent = React.createClass({
           <h1>Alazen</h1>
         </div>
         <div className="chromosome-info">
-          <h2>Chromosome 14</h2>
+          <h2 id="chromosome_header">Chromosome 1</h2>
         </div>
 
         <div className="container-fluid main">
+          <div id="chromosome_selection_component"></div>
+            <SelectChromosome list={chromosomeList} selected={chromosomeList[0]} />
           <div className="row navigation">
             <div className="col-xs-12 col-sm-7">
               <SearchField/>
