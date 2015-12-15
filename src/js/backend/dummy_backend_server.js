4e9bc25b00d5e0c9c0bc4e0e99449139c2858620
lrebscher <lucas.rebscher@sprylab.com>
2015-12-15 18:57:35 +0100

    * fix wrong-command in install-description * add missing semicolons

--- src/js/backend/dummy_backend_server.js
+++ src/js/backend/dummy_backend_server.js
@@ -1,8 +1,8 @@
 /*
-* using express (web-framework for node.js)
-* express needs to be installed -> node install express
-* then run 'node dummy_backend_server.js' to start the server
-*/
+ * using express (web-framework for node.js)
+ * express needs to be installed -> npm install express
+ * then run 'node dummy_backend_server.js' to start the server
+ */
 var express = require('express');
 var app = express();
 var fs = require("fs");
@@ -10,26 +10,25 @@ var fs = require("fs");
 console.log("starting dummy backend...");
 
 app.get('/refGen', function (req, res) {
-      fs.readFile("../../static/test/dummyRefGen.json", 'utf8', function (err, data) {
-      console.log(data);
-      res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1');
-      res.jsonp(JSON.stringify(data));
-      res.end();
-   });
+    fs.readFile("../../static/test/dummyRefGen.json", 'utf8', function (err, data) {
+        console.log(data);
+        res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1');
+        res.jsonp(JSON.stringify(data));
+        res.end();
+    });
 });
 
 app.get('/mutations', function (req, res) {
-      fs.readFile("../../static/test/dummyMutations.json", 'utf8', function (err, data) {
-       console.log(data);
-       res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1');
-       res.jsonp(JSON.stringify(data));
-       res.end();
-   });
+    fs.readFile("../../static/test/dummyMutations.json", 'utf8', function (err, data) {
+        console.log(data);
+        res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1');
+        res.jsonp(JSON.stringify(data));
+        res.end();
+    });
 });
 
 var server = app.listen(8081, 'localhost', function () {
-
-  var host = server.address().address
-  var port = server.address().port
-  console.log("dummy-backend-server listening at http://%s:%s", host, port)
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log("dummy-backend-server listening at http://%s:%s", host, port)
 });
