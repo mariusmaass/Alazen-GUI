bf7e046a7c2210345a064a6f68fff5b1c8a01d51
ismirlatte <lucas.rebscher@web.de>
2016-01-08 21:21:34 +0100

    fix data_provider_spec tests

--- src/js/spec/data_provider_spec.js
+++ src/js/spec/data_provider_spec.js
@@ -5,11 +5,19 @@ import dataProvider from 'backend/data_provider';
 describe('dataProvider', function() {
   describe('[actions]', function() {
     it('has a getSource function', function()  {
-      expect(dataProvider.getSource).toBeDefined();
+      expect(dataProvider.getPosition).toBeDefined();
+    });
+
+    it('has a getInitialSources function,', function() {
+      expect(dataProvider.getInitialSources).toBeDefined();
     });
 
     it('has a searchGene function', function() {
       expect(dataProvider.searchGene).toBeDefined();
+    });
+
+    it('has a getPossibleGeneNames function', function() {
+      expect(dataProvider.getPossibleGeneNames).toBeDefined();
     });
   });
 });
