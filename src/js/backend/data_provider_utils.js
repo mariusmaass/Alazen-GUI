e5d702389c4e90fc0dce13efa2b42104feb6f69e
ismirlatte <lucas.rebscher@web.de>
2016-01-12 12:28:57 +0100

    fix formatting

--- src/js/backend/data_provider_utils.js
+++ src/js/backend/data_provider_utils.js
@@ -24,19 +24,19 @@ var providerUtils = {
       });
       return mutationSequence;
     }
-    for (var index = 0; index < mutations.length; index++) {
-      startOfMutation = mutations[index].interval.from;    
+    for(var index = 0; index < mutations.length; index++) {
+      startOfMutation = mutations[index].interval.from;
       if (startOfMutation != 0 && index == 0) {
-    	  sequence = ref.substring(endOfLastInterval, startOfMutation);
-    	  pushSequence(sequence, mutationSequence, mutationSequenceIndex);
-    	  mutationSequenceIndex++;
+        sequence = ref.substring(endOfLastInterval, startOfMutation);
+        pushSequence(sequence, mutationSequence, mutationSequenceIndex);
+        mutationSequenceIndex++;
       } else if (endOfLastInterval < startOfMutation - 1) {
-    	   sequence = ref.substring(endOfLastInterval + 1, startOfMutation);
-    	   pushSequence(sequence, mutationSequence, mutationSequenceIndex);
-    	   mutationSequenceIndex++;
+        sequence = ref.substring(endOfLastInterval + 1, startOfMutation);
+        pushSequence(sequence, mutationSequence, mutationSequenceIndex);
+        mutationSequenceIndex++;
       }
       endOfLastInterval = mutations[index].interval.to;
-      var subSequence = ref.substring(startOfMutation, endOfLastInterval+1);
+      var subSequence = ref.substring(startOfMutation, endOfLastInterval + 1);
       mutationSequence.push({
         id: mutationSequenceIndex,
         sequence: subSequence,
@@ -44,10 +44,10 @@ var providerUtils = {
         metadata: mutations[index].metadata
       });
       mutationSequenceIndex++;
-      if (index + 1 == mutations.length && endOfLastInterval != ref.length-1) {
-    	  sequence = ref.substring(endOfLastInterval+1, ref.length);
-    	  pushSequence(sequence, mutationSequence, mutationSequenceIndex);
-    	  mutationSequenceIndex++;
+      if (index + 1 == mutations.length && endOfLastInterval != ref.length - 1) {
+        sequence = ref.substring(endOfLastInterval + 1, ref.length);
+        pushSequence(sequence, mutationSequence, mutationSequenceIndex);
+        mutationSequenceIndex++;
       }
     }
     return mutationSequence;
