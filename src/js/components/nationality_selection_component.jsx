e1c586276d11bc6ad68f9ed50f95112bc7950f23
AleksandarAtanasov91 <aleksandar_atanasov91@abv.bg>
2016-01-15 21:35:13 +0100

    Multiple nationality selection dropdown working properly with testlist

---
+++ src/js/components/nationality_selection_component.jsx
@@ -0,0 +1,42 @@
+'use strict';
+import React from 'react';
+var SelectNationality = React.createClass({
+  getSelectValues: function(item) {
+    var select = this.refs.selectBox;
+    var result = [];
+    var options = select && select.options;
+    var opt;
+
+    for (var i = 0, iLen = options.length; i < iLen; i++) {
+      opt = options[i];
+
+      if (opt.selected) {
+        result.push(opt.value || opt.text);
+      }
+    }
+    console.log("Nationality " + result + " selected.");
+    this.props.nationalitySelected(result);
+  },
+
+  render: function() {
+    return (
+        <div className="country-selection-list">
+            <select multiple className="form-control" ref="selectBox" onChange={this.getSelectValues}>
+                {this.renderListCountries()}
+            </select>
+        </div>
+    );
+  },
+
+  renderListCountries: function() {
+    var countries = [];
+    for (var i = 0; i < this.props.list.length; i++) {
+      var country = this.props.list[i];
+      countries.push(
+        <option value={country.id} key={country.id}>{country.name}</option>
+      );
+    }
+    return countries;
+  }
+});
+export default SelectNationality;
