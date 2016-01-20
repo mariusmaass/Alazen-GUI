7988fc47715e672b036e3481478a9c04246a7f78
Robin Papke <robin_papke@web.de>
2016-01-20 20:33:02 +0100

    summarized test cases for function buildMutationSequence

--- src/js/spec/data_provider_utils_spec.js
+++ src/js/spec/data_provider_utils_spec.js
@@ -4,88 +4,6 @@ import providerUtils from 'backend/data_provider_utils';
 
 var testMutationJSON =
 '{"details": ' +
-  '{"refseq": "AAACCCGGGTTT",' +
-  '"mutations": [' +
-    '{"refname":"PX7",' +
-    '"mutationseq": "CCG","position":' +
-    '{"from": 4, "to": 6 },' +
-    '"metadata": ""},' +
-    '{"refname": "PX10",' +
-    '"mutationseq": "GGT", "position":' +
-    '{"from": 7, "to": 9},' +
-    '"metadata": ""}' +
-  ']}' +
-'}';
-
-var expectedAnswer = new Array({
-  id: 0,
-  sequence: "AAAC",
-  mutationFlag: false,
-  metadata: ""
-  },
-  {
-    id: 1,
-    sequence: "CCG",
-    mutationFlag: true,
-    metadata: ""
-  },
-  {
-    id: 2,
-    sequence: "GGT",
-    mutationFlag: true,
-    metadata: ""
-  },
-  {
-    id: 3,
-    sequence: "TT",
-    mutationFlag: false,
-    metadata: ""
-  }
-);
-
-var testMutationJSON2 =
-'{"details": ' +
-  '{"refseq": "AAACCCCGGGGTTTT",' +
-  '"mutations": [' +
-    '{"refname":"PX7",' +
-    '"mutationseq": "CCGGGG","position":' +
-    '{"from": 5, "to": 10 },' +
-    '"metadata": ""},' +
-    '{"refname": "PX10",' +
-    '"mutationseq": "GGTTT", "position":' +
-    '{"from": 9, "to": 13},' +
-    '"metadata": ""}' +
-  ']}' +
-'}';
-
-var expectedAnswer2 = new Array({
-  id: 0,
-  sequence: "AAACC",
-  mutationFlag: false,
-  metadata: ""
-  },
-  {
-    id: 1,
-    sequence: "CCGGGG",
-    mutationFlag: true,
-    metadata: ""
-  },
-  {
-    id: 2,
-    sequence: "GGTTT",
-    mutationFlag: true,
-    metadata: ""
-  },
-  {
-    id: 3,
-    sequence: "T",
-    mutationFlag: false,
-    metadata: ""
-  }
-);
-
-var testMutationJSON3 =
-'{"details": ' +
   '{"refseq": "AAACCCCGGGGTTTT",' +
   '"mutations": [' +
     '{"refname":"PX7",' +
@@ -99,7 +17,7 @@ var testMutationJSON3 =
   ']}' +
 '}';
 
-var expectedAnswer3 = new Array({
+var expectedAnswer = new Array({
   id: 0,
   sequence: "AAACCCC",
   mutationFlag: true,
@@ -125,10 +43,6 @@ describe('dataProvider', function() {
     it('has a buildMutationSequence function', function() {
       var answer = providerUtils.buildMutationSequence(JSON.parse(testMutationJSON));
       expect(JSON.stringify(answer)).toBe(JSON.stringify(expectedAnswer));
-      answer = providerUtils.buildMutationSequence(JSON.parse(testMutationJSON2));
-      expect(JSON.stringify(answer)).toBe(JSON.stringify(expectedAnswer2));
-      answer = providerUtils.buildMutationSequence(JSON.parse(testMutationJSON3));
-      expect(JSON.stringify(answer)).toBe(JSON.stringify(expectedAnswer3));
     });
   });
 });
